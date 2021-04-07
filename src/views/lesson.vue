ec602b8f44f8f41430cd16028c698eb8ac131cde
Huy Hoang Nguyen <hoangnh0099@gmail.com>
2021-04-07 16:39:54 +0700

    feat: sort and search

--- src/views/lesson.vue
+++ src/views/lesson.vue
@@ -1,22 +1,35 @@
 <template>
   <Layout>
-    <v-data-table
-      :headers="headers"
-      :items="desserts"
-      :items-per-page="5"
-      class="elevation-1"
-    >
-      <template v-slot:item.actions="{ item }">
-        <v-icon small class="mr-2" @click="editItem(item)"> mdi-pencil </v-icon>
-        <v-icon small class="mr-2" @click="deleteItem(item)">
-          mdi-delete
-        </v-icon>
-        <v-icon small @click="deleteItem(item)"> mdi-information </v-icon>
-      </template>
-      <template v-slot:no-data>
-        <v-btn color="primary" @click="initialize"> Reset </v-btn>
-      </template></v-data-table
-    >
+    <v-card>
+      <v-card-title>
+        <v-text-field
+          v-model="search"
+          append-icon="mdi-magnify"
+          label="Search"
+          single-line
+          hide-details
+        ></v-text-field>
+      </v-card-title>
+      <v-data-table
+        :headers="headers"
+        :items="desserts"
+        :items-per-page="5"
+        class="elevation-1"
+      >
+        <template v-slot:item.actions="{ item }">
+          <v-icon small class="mr-2" @click="editItem(item)">
+            mdi-pencil
+          </v-icon>
+          <v-icon small class="mr-2" @click="deleteItem(item)">
+            mdi-delete
+          </v-icon>
+          <v-icon small @click="deleteItem(item)"> mdi-information </v-icon>
+        </template>
+        <template v-slot:no-data>
+          <v-btn color="primary" @click="initialize"> Reset </v-btn>
+        </template></v-data-table
+      >
+    </v-card>
   </Layout>
 </template>
 
@@ -32,14 +45,13 @@ export default Vue.extend({
       {
         text: "Chủ đề",
         align: "start",
-        sortable: false,
         value: "name",
       },
       { text: "Actions", value: "actions" },
     ],
     desserts: [
       {
-        name: "Frozen Yogurt",
+        name: "Frozen Yogurt - 1",
       },
       {
         name: "Frozen Yogurt",
